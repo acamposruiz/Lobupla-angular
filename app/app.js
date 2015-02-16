4b897bd3c5c48c7a104efb5a3ffdaf825a1e9c98
Antonio Campos <antonio.ciber@ciber.com>
2015-02-16 16:42:32 +0100

    start with node

--- app/app.js
+++ app/app.js
@@ -6,6 +6,9 @@ var app = express();
 var server = http.createServer(app);
 
 app.use('/styles', express.static(__dirname + '/styles')); 
+app.use('/scripts', express.static(__dirname + '/scripts')); 
+app.use('/views', express.static(__dirname + '/views')); 
+app.use('/bower_components', express.static(__dirname + '/bower_components')); 
 
 app.get('/', function(req, res){
   //res.send('Hello World!!');
