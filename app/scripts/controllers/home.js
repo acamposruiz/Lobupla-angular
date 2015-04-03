ae43a7a799fedae250671defa173c1d6bdcb1e21
Antonio Campos <acamposruiz@gmail.com>
2015-04-03 12:42:03 +0200

    preredesign

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,107 +1,105 @@
-
-
 angular.module('lobuplaApp')
-  .directive('ngEnter', function () {
-    'use strict';
-    return function (scope, element, attrs) {
-      element.bind("keydown keypress", function (event) {
-        if(event.which === 13) {
-          scope.$apply(function (){
-            scope.$eval(attrs.ngEnter);
-          });
-          event.preventDefault();
-        }
+.directive('ngEnter', function () {
+  'use strict';
+  return function (scope, element, attrs) {
+    element.bind("keydown keypress", function (event) {
+      if(event.which === 13) {
+        scope.$apply(function (){
+          scope.$eval(attrs.ngEnter);
+        });
+        event.preventDefault();
+      }
+    });
+  };
+})
+.factory('getCoordinates', function($http, $q) {
+  'use strict';
+  return{
+    fromAddress : function(address) {
+      var deferred = $q.defer();
+      $http({
+        url: 'http://maps.googleapis.com/maps/api/geocode/json',
+        params: {
+          address: address,
+          components:'country:ES'
+        },
+        method: 'GET',
+        //data: data,
+        headers: angular.extend({
+          'X-Requested-With': undefined
+        })
+      }).
+      success(function(data){
+        deferred.resolve(data.results[0].geometry.location.lat + ',' + data.results[0].geometry.location.lng);
       });
-    };
-  })
-  .factory('getCoordinates', function($http, $q) {
-    'use strict';
-    return{
-      fromAddress : function(address) {
-        var deferred = $q.defer();
-        $http({
-          url: 'http://maps.googleapis.com/maps/api/geocode/json',
-          params: {
-            address: address,
-            components:'country:ES'
-          },
-          method: 'GET',
-          //data: data,
-          headers: angular.extend({
-            'X-Requested-With': undefined
-          })
-        }).
-        success(function(data){
-          deferred.resolve(data.results[0].geometry.location.lat + ',' + data.results[0].geometry.location.lng);
-        });
-        return deferred.promise;
-      }
-    };
-  })
-  .factory('getVenues', function($http, $q) {
-    'use strict';
-    return{
-      fromCoordinates : function(coordinates) {
-        var deferred = $q.defer();
-        $http({
-          url: 'https://api.foursquare.com/v2/venues/search',
-          params: {
-            client_id:'WU3OIROB5N3J1U3JPWWP0EUVICAZTMDCL2MUFLM2RKZ4HZFO',
-            client_secret:'YF3BCWYDRXLSRUOSJ24WDBKWFZMYDGS1EYF5TSHM2O35VACU',
-            ll: coordinates,
-            v: '20150217'
-          },
-          method: 'GET',
-          //data: data,
-          headers: angular.extend({
-            'X-Requested-With': undefined
-          })
-        }).
-        success(function(data){
-          deferred.resolve(data.response.venues);
-        });
-        return deferred.promise;
-      }
-    };
-  })
-  .controller('HomeCtrl', function ($scope, $cookies, $q, getCoordinates, getVenues) {
-    'use strict';
-    $scope.showLatest = ($cookies.showLatest === 'true')? true: false;
-    $scope.lastestSearchs = ($cookies.lastestSearchs)? JSON.parse($cookies.lastestSearchs): null;
-
-    $scope.venues = {
-      search: function(address) {
-        $scope.address = address;
-        $scope.venues.searchCache.updateLastestSearchs(address);
-
-        getCoordinates.fromAddress(address)
-          .then(getVenues.fromCoordinates)
-          .then(function(venues) {
-            $scope.venues.context = venues;
-          });
-      },
-      searchCache: {
-        updateLastestSearchs: function(address) {
-          if ($cookies.lastestSearchs) {
-            $scope.lastestSearchs = JSON.parse($cookies.lastestSearchs);
-            if ($scope.lastestSearchs.indexOf(address) !== -1) {
-              $scope.lastestSearchs.splice($scope.lastestSearchs.indexOf(address),1);
-            }
-            $scope.lastestSearchs.unshift(address);
-            $cookies.lastestSearchs = JSON.stringify($scope.lastestSearchs);
-          }
-          else{
-            $cookies.lastestSearchs = JSON.stringify([address]);
-          }
+      return deferred.promise;
+    }
+  };
+})
+.factory('getVenues', function($http, $q) {
+  'use strict';
+  return{
+    fromCoordinates : function(coordinates) {
+      var deferred = $q.defer();
+      $http({
+        url: 'https://api.foursquare.com/v2/venues/search',
+        params: {
+          client_id:'WU3OIROB5N3J1U3JPWWP0EUVICAZTMDCL2MUFLM2RKZ4HZFO',
+          client_secret:'YF3BCWYDRXLSRUOSJ24WDBKWFZMYDGS1EYF5TSHM2O35VACU',
+          ll: coordinates,
+          v: '20150217'
         },
-        toggleLatest: function(){
-          $scope.showLatest = !$scope.showLatest;
-          $cookies.showLatest = $scope.showLatest.toString();
-        },
-        removeAddress: function(index) {
-          $scope.lastestSearchs.splice(index,1);
+        method: 'GET',
+        //data: data,
+        headers: angular.extend({
+          'X-Requested-With': undefined
+        })
+      }).
+      success(function(data){
+        deferred.resolve(data.response.venues);
+      });
+      return deferred.promise;
+    }
+  };
+})
+.controller('HomeCtrl', function ($scope, $cookies, $q, getCoordinates, getVenues) {
+  'use strict';
+  $scope.showLatest = ($cookies.showLatest === 'true')? true: false;
+  $scope.lastestSearchs = ($cookies.lastestSearchs)? JSON.parse($cookies.lastestSearchs): null;
+
+  $scope.venues = {
+    search: function(address) {
+      $scope.address = address;
+      $scope.venues.searchCache.updateLastestSearchs(address);
+
+      getCoordinates.fromAddress(address)
+        .then(getVenues.fromCoordinates)
+        .then(function(venues) {
+          $scope.venues.context = venues;
+        });
+    },
+    searchCache: {
+      updateLastestSearchs: function(address) {
+        if ($cookies.lastestSearchs) {
+          $scope.lastestSearchs = JSON.parse($cookies.lastestSearchs);
+          if ($scope.lastestSearchs.indexOf(address) !== -1) {
+            $scope.lastestSearchs.splice($scope.lastestSearchs.indexOf(address),1);
+          }
+          $scope.lastestSearchs.unshift(address);
           $cookies.lastestSearchs = JSON.stringify($scope.lastestSearchs);
         }
+        else{
+          $cookies.lastestSearchs = JSON.stringify([address]);
+        }
+      },
+      toggleLatest: function(){
+        $scope.showLatest = !$scope.showLatest;
+        $cookies.showLatest = $scope.showLatest.toString();
+      },
+      removeAddress: function(index) {
+        $scope.lastestSearchs.splice(index,1);
+        $cookies.lastestSearchs = JSON.stringify($scope.lastestSearchs);
       }
-    };
-  });
+    }
+  };
+});
--- test/spec/protactor-spec.js
+++ test/spec/protactor-spec.js
@@ -1,19 +1,20 @@
-'use strict';
+
 
 describe('first search', function() {
-    beforeEach(function() {
-        browser.get('http://localhost:9089/#/home');
+  'use strict';
+  beforeEach(function() {
+    browser.get('http://localhost:9089/#/home');
 
-        element(by.model('address')).sendKeys('Málaga');
-        element(by.css('#send')).click();
-        browser.sleep(5000);
-    });
+    element(by.model('address')).sendKeys('Málaga');
+    element(by.css('#send')).click();
+    browser.sleep(5000);
+  });
 
-    it ('Check results', function(){
-        var todoList = element.all(by.repeater('item in venues'));
-        expect(todoList.count()).toEqual(30);
+  it ('Check results', function(){
+    var todoList = element.all(by.repeater('item in venues'));
+    expect(todoList.count()).toEqual(30);
 
-    });
+  });
 
   // it('Looking for Málaga', function() {
   //   browser.get('http://localhost:9089/api.lobupla2/app/#/home');
