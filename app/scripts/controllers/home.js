19b4c6d2c2050d982f608b5733bfeea590f8f3fe
Antonio Campos <acamposruiz@gmail.com>
2015-04-05 16:49:07 +0200

    Cstrgory buttons

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -17,7 +17,7 @@ angular.module('lobuplaApp')
   var client_id = 'WU3OIROB5N3J1U3JPWWP0EUVICAZTMDCL2MUFLM2RKZ4HZFO';
   var client_secret = 'YF3BCWYDRXLSRUOSJ24WDBKWFZMYDGS1EYF5TSHM2O35VACU';
   var apiVersion = '20150217';
-  var coordinatesFromAddress = function(address) {
+  var coordinatesFromAddress = function(address, categoryId) {
     var deferred = $q.defer();
     $http({
       url: 'http://maps.googleapis.com/maps/api/geocode/json',
@@ -32,18 +32,22 @@ angular.module('lobuplaApp')
       })
     }).
       success(function(data){
-        deferred.resolve(data.results[0].geometry.location.lat + ',' + data.results[0].geometry.location.lng);
+        deferred.resolve({
+          coordinates: data.results[0].geometry.location.lat + ',' + data.results[0].geometry.location.lng,
+          categoryId:categoryId
+        });
       });
     return deferred.promise;
   };
-  var venuesFromCoordinates = function(coordinates) {
+  var venuesFromCoordinates = function(data) {
     var deferred = $q.defer();
     $http({
       url: 'https://api.foursquare.com/v2/venues/search',
       params: {
         client_id: client_id,
         client_secret: client_secret,
-        ll: coordinates,
+        ll: data.coordinates,
+        categoryId: data.categoryId,
         v: apiVersion
       },
       method: 'GET',
@@ -59,8 +63,8 @@ angular.module('lobuplaApp')
   };
 
   return{
-    venuesFromAddress:function(address){
-      return coordinatesFromAddress(address).then(venuesFromCoordinates);
+    get: function(address, categoryId){
+      return coordinatesFromAddress(address, categoryId).then(venuesFromCoordinates);
     },
     getImages: function(venue, size) {
       var deferred = $q.defer();
@@ -101,10 +105,53 @@ angular.module('lobuplaApp')
 .controller('HomeCtrl', function ($scope, $cookies, $q, getVenues) {
   'use strict';
 
+  $scope.$root.categoryId = '4d4b7105d754a06374d81259';
+  $scope.$root.categories = [
+    {
+      name:'Arte y entretenimiento',
+      id:'4d4b7104d754a06370d81259'
+    },
+    {
+      name:'Facultad y Universidad',
+      id:'4d4b7105d754a06372d81259'
+    },
+    {
+      name:'Eventos',
+      id:'4d4b7105d754a06373d81259'
+    },
+    {
+      name:'Comida',
+      id:'4d4b7105d754a06374d81259'
+    },
+    {
+      name:'Local nocturno',
+      id:'4d4b7105d754a06376d81259'
+    },
+    {
+      name:'Aire libre y recreación',
+      id:'4d4b7105d754a06377d81259'
+    },
+    {
+      name:'Profesionales y otros sitios',
+      id:'4d4b7105d754a06375d81259'
+    },
+    {
+      name:'Residencia',
+      id:'4e67e38e036454776db1fb3a'
+    },
+    {
+      name:'Tienda y servicio',
+      id:'4d4b7105d754a06378d81259'
+    },
+    {
+      name:'Viajes y Transporte',
+      id:'4d4b7105d754a06379d81259'
+    }
+  ];
   $scope.$root.venues = {
-    search: function(address) {
+    search: function(address, categoryId) {
       $scope.address = address;
-      getVenues.venuesFromAddress(address)
+      getVenues.get(address, categoryId)
         .then(function(venues) {
           $scope.venues.context = venues;
           angular.forEach(venues, function(venue, key) {
