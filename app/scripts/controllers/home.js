e8e6de2dd3d51c50aeb5f2b9d77eab96d3d82d19
Antonio Campos <acamposruiz@gmail.com>
2015-04-05 15:14:06 +0200

    Refactoring

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -17,47 +17,50 @@ angular.module('lobuplaApp')
   var client_id = 'WU3OIROB5N3J1U3JPWWP0EUVICAZTMDCL2MUFLM2RKZ4HZFO';
   var client_secret = 'YF3BCWYDRXLSRUOSJ24WDBKWFZMYDGS1EYF5TSHM2O35VACU';
   var apiVersion = '20150217';
-
-  return{
-    coordinatesFromAddress: function(address) {
-      var deferred = $q.defer();
-      $http({
-        url: 'http://maps.googleapis.com/maps/api/geocode/json',
-        params: {
-          address: address,
-          components:'country:ES'
-        },
-        method: 'GET',
-        //data: data,
-        headers: angular.extend({
-          'X-Requested-With': undefined
-        })
-      }).
-        success(function(data){
-          deferred.resolve(data.results[0].geometry.location.lat + ',' + data.results[0].geometry.location.lng);
-        });
-      return deferred.promise;
-    },
-    venuesFromCoordinates: function(coordinates) {
-      var deferred = $q.defer();
-      $http({
-        url: 'https://api.foursquare.com/v2/venues/search',
-        params: {
-          client_id: client_id,
-          client_secret: client_secret,
-          ll: coordinates,
-          v: apiVersion
-        },
-        method: 'GET',
-        //data: data,
-        headers: angular.extend({
-          'X-Requested-With': undefined
-        })
-      }).
+  var coordinatesFromAddress = function(address) {
+    var deferred = $q.defer();
+    $http({
+      url: 'http://maps.googleapis.com/maps/api/geocode/json',
+      params: {
+        address: address,
+        components:'country:ES'
+      },
+      method: 'GET',
+      //data: data,
+      headers: angular.extend({
+        'X-Requested-With': undefined
+      })
+    }).
+      success(function(data){
+        deferred.resolve(data.results[0].geometry.location.lat + ',' + data.results[0].geometry.location.lng);
+      });
+    return deferred.promise;
+  };
+  var venuesFromCoordinates = function(coordinates) {
+    var deferred = $q.defer();
+    $http({
+      url: 'https://api.foursquare.com/v2/venues/search',
+      params: {
+        client_id: client_id,
+        client_secret: client_secret,
+        ll: coordinates,
+        v: apiVersion
+      },
+      method: 'GET',
+      //data: data,
+      headers: angular.extend({
+        'X-Requested-With': undefined
+      })
+    }).
       success(function(data){
         deferred.resolve(data.response.venues);
       });
-      return deferred.promise;
+    return deferred.promise;
+  };
+
+  return{
+    venuesFromAddress:function(address){
+      return coordinatesFromAddress(address).then(venuesFromCoordinates);
     },
     getImages: function(venue, size) {
       var deferred = $q.defer();
@@ -74,23 +77,23 @@ angular.module('lobuplaApp')
           'X-Requested-With': undefined
         })
       }).
-        success(function(data){
-          try {
-            if(data.response.photos.count > 0){
-              var images = [];
-              angular.forEach(data.response.photos.items, function(item, key) {
-                images.push(item.prefix + size + item.suffix);
-              });
-              deferred.resolve(images);
-            }
-            else {
-              deferred.resolve(null);
-            }
+      success(function(data){
+        try {
+          if(data.response.photos.count > 0){
+            var images = [];
+            angular.forEach(data.response.photos.items, function(item, key) {
+              images.push(item.prefix + size + item.suffix);
+            });
+            deferred.resolve(images);
           }
-          catch(err) {
+          else {
             deferred.resolve(null);
           }
-        });
+        }
+        catch(err) {
+          deferred.resolve(null);
+        }
+      });
       return deferred.promise;
     }
   };
@@ -101,9 +104,7 @@ angular.module('lobuplaApp')
   $scope.$root.venues = {
     search: function(address) {
       $scope.address = address;
-
-      getVenues.coordinatesFromAddress(address)
-        .then(getVenues.venuesFromCoordinates)
+      getVenues.venuesFromAddress(address)
         .then(function(venues) {
           $scope.venues.context = venues;
           angular.forEach(venues, function(venue, key) {
