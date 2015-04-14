df83798abe10b82cd8577a95b09f12b30092708e
Antonio Campos <acamposruiz@gmail.com>
2015-04-14 22:40:52 +0200

    Export integration foursquare and google API to bower packages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,16 @@
- angular.module('lobuplaApp', [
+angular.module('lobuplaApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ui.router'
+  'ui.router',
+  'angular-foursquare-api'
 ])
+  .value('angular_foursquare_conf', {
+    client_id: 'WU3OIROB5N3J1U3JPWWP0EUVICAZTMDCL2MUFLM2RKZ4HZFO',
+    client_secret: 'YF3BCWYDRXLSRUOSJ24WDBKWFZMYDGS1EYF5TSHM2O35VACU'
+  })
   .config(function ($stateProvider, $urlRouterProvider) {
-      'use strict';
+    'use strict';
     //delete $httpProvider.defaults.headers.common['X-Requested-With'];
     $urlRouterProvider.otherwise('/');
     $stateProvider
